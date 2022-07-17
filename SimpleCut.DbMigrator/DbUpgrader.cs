829028ca918c52bd37d82ab4ef5353526e074b47
noeducation <noeducationvp@gmail.com>
2022-07-17 21:06:08 +0200

    Added DbMigrator

---
+++ SimpleCut.DbMigrator/DbUpgrader.cs
@@ -0,0 +1,36 @@
+﻿using DbUp;
+using DbUp.Engine;
+
+namespace SimpleCut.DbMigrator
+{
+    public class DbUpgrader
+    {
+        public List<SqlScript> allExecutedScripts = new List<SqlScript>();
+
+        public DatabaseUpgradeResult UpgradeDatabase(string connectionString, string path)
+        {
+            EnsureDatabase.For.PostgresqlDatabase(connectionString);
+
+            DatabaseUpgradeResult upgradeResult = PerformUpgrade(connectionString, path);
+
+            return new DatabaseUpgradeResult(allExecutedScripts, upgradeResult.Successful, upgradeResult.Error, upgradeResult.ErrorScript);
+        }
+
+        private DatabaseUpgradeResult PerformUpgrade(string connectionString, string filesInPath)
+        {
+            var builder = DeployChanges
+                .To
+                .PostgresqlDatabase(connectionString)
+                .LogToConsole()
+                .WithTransaction()
+                .WithVariablesDisabled()
+                .WithScriptsFromFileSystem(filesInPath);
+
+            var result = builder.Build().PerformUpgrade();
+
+            allExecutedScripts.AddRange(result.Scripts);
+
+            return result;
+        }
+    }
+}
---
+++ SimpleCut.DbMigrator/Program.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace SimpleCut.DbMigrator
+{
+    public class Program
+    {
+        private static string _connectionName = "DefaultConnection";
+        private static IConfiguration? _configuration;
+
+        static int Main(string[] args)
+        {
+            var connectionString = GetConnectionString();
+
+            var upgrader = new DbUpgrader();
+
+            try
+            {
+                Console.WriteLine("DB Update connectionString: " + connectionString);
+
+                var result = upgrader.UpgradeDatabase(connectionString,
+                    Path.Combine(Directory.GetCurrentDirectory(), "Scripts"));
+
+                if (!result.Successful)
+                {
+                    FastHandleError(result.Error);
+                }
+
+                foreach (var script in result.Scripts)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine($"Executed script: {script.Name}");
+                }
+            }
+            catch (Exception ex)
+            {
+                FastHandleError(ex);
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Db upgrade success!");
+            Console.ResetColor();
+
+            #if DEBUG
+                Console.ReadLine();
+            #endif
+
+            return 0;
+        }
+
+        private static void FastHandleError(Exception ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(ex);
+            Console.ResetColor();
+
+            #if DEBUG
+                Console.ReadLine();
+            #endif
+
+            using (var errWriter = Console.Error)
+            {
+                errWriter.WriteLine(ex);
+                errWriter.Flush();
+            }
+
+            Environment.Exit(-1);
+        }
+
+        private static string GetConnectionString()
+        {
+            _configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", false)
+                .AddEnvironmentVariables()
+                .Build();
+
+            return _configuration.GetConnectionString(_connectionName);
+        }
+
+    }
+}
